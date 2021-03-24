5725897a463e5c753bd8e49bc7ade6667d2ad239
pirvoaica.daniel.g2t <pirvoaica.daniel.g2t@student.ucv.ro>
2021-03-24 23:19:40 +0200

    ESlint rule add

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     'react/jsx-props-no-spreading': 'off',
     'no-nested-ternary': 'off',
     'react/prop-types': 'off',
