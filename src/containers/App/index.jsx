2f45fa19bde38297c76e3b3bac7d952d08f0ccd3
pirvoaica.daniel.g2t <pirvoaica.daniel.g2t@student.ucv.ro>
2021-04-10 23:50:41 +0300

    Redirect user to login if not logged in

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter, Redirect, Route, Switch,
+} from 'react-router-dom';
 import '../../scss/style.scss';
 import { useInjectReducer } from 'redux-injectors';
+import { useSelector } from 'react-redux';
 import Layout from '../Theme/Layout/Loadable';
 import Login from '../Security/Login/Loadable';
 import Register from '../Security/Register/Loadable';
 import NotFoundPage from '../NotFoundPage/Loadable';
 import LoadingIndicator from '../../components/LoadingIndicator';
 import { reducer, sliceKey } from './slice';
+import { selectUser } from './selectors';
 
 const App = () => {
   useInjectReducer({ key: sliceKey, reducer });
   // useInjectSaga({ key: sliceKey, saga: appSaga });
+  const user = useSelector(selectUser);
   return (
     <BrowserRouter>
       <React.Suspense fallback={LoadingIndicator}>
@@ -19,7 +24,7 @@ const App = () => {
           <Route exact path="/login" name="Login Page" render={(props) => <Login {...props} />} />
           <Route exact path="/register" name="Register Page" render={(props) => <Register {...props} />} />
           <Route exact path="/404" name="Page 404" render={(props) => <NotFoundPage {...props} />} />
-          <Route path="/" name="Home" render={(props) => <Layout {...props} />} />
+          <Route path="/" name="Home" render={(props) => (user ? <Layout {...props} /> : <Redirect to="/login" />)} />
         </Switch>
       </React.Suspense>
     </BrowserRouter>
